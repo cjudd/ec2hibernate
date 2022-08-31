61d95590f4745ee69d0f88a2bc65a47064eaaab5
Christopher M. Judd <javajudd@gmail.com>
2022-08-31 14:10:04 -0400

    Initial creation using start.spring.io with no dependencies.

---
+++ src/main/java/net/javajudd/aws/ec2hibernate/Application.java
@@ -0,0 +1,13 @@
+package net.javajudd.aws.ec2hibernate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
---
+++ src/test/java/net/javajudd/aws/ec2hibernate/ApplicationTests.java
@@ -0,0 +1,13 @@
+package net.javajudd.aws.ec2hibernate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
